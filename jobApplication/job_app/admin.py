138c30128dc3e89910b92bd172849ff4ff370e07
Abhisingh980 <kr.abhinesh147@gmail.com>
2024-02-07 21:09:21 +0530

    add navigation bar admin panel of django and som modification on admin.py

--- jobApplication/job_app/admin.py
+++ jobApplication/job_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Form
+
+
+class FormsAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email",)
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("data", "occupation")
+    ordering = ("first_name",)
+    readonly_fields = ("occupation", "data", "email")
+
+
+admin.site.register(Form, FormsAdmin)
--- jobApplication/job_app/views.py
+++ jobApplication/job_app/views.py
@@ -6,20 +6,23 @@ from django.core.mail import EmailMessage
 
 
 def index(request):
-    if request == 'POST':
-        forms = ApplicationForm()
-        if forms.is_valid():
-            first_name = forms.cleaned_data["first_name"]
-            last_name = forms.cleaned_data["last_name"]
-            email = forms.cleaned_data["email"]
-            date = forms.cleaned_data["data"]
-            occupation = forms.cleaned_data["occupation"]
-
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            occupation = form.cleaned_data["occupation"]
             Form.objects.create(first_name=first_name, last_name=last_name,
-                                email=email, date=date, occupation=occupation)
+                                email=email, data=date, occupation=occupation)
             messages_body = f"Thanks for submitting  {first_name}."
             email_message = EmailMessage("From submission confirmation", messages_body,
                                          to=[email])
             email_message.send()
             messages.success(request, "Forms submitted successfully!")
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request, 'about.html')
